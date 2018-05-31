b1b21bc7a216627ba72cd62d4fb0fb9a3ab1a5fa
Rurik Pinheiro <rurikspin@gmail.com>
2018-05-31 12:02:28 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');				
+const CleanWebpackPlugin = require('clean-webpack-plugin');	
+const LinkMediaHtmlWebpackPlugin = require('link-media-html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        }
+    ]
+  }
+};
